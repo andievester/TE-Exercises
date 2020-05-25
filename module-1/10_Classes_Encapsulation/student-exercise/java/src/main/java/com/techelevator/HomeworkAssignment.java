3eb86c0110a72344472580bd04a1a1e4869b4820
Andie Vester <avester146@gmail.com>
2020-05-25 12:59:54 -0400

    Submitting Week 2 Day 5 Exercises

--- module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/10_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,44 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+	private int earnedMarks;
+	private int possibleMarks;
+	private String submitterName;
+	
+	public String getLetterGrade() {
+		if ((((double)earnedMarks / possibleMarks) * 100) >= 90) {
+			return "A";
+		} else if ((((double)earnedMarks / possibleMarks) * 100) >= 80) {
+			return "B";
+		} else if ((((double)earnedMarks / possibleMarks) * 100) >= 70) {
+			return "C";
+		} else if ((((double)earnedMarks / possibleMarks) * 100) >= 60) {
+			return "D";
+		} else {
+			return "F";
+		}	
+	}
+	
+	
+	public HomeworkAssignment (int possibleMarks) {
+		this.possibleMarks = possibleMarks;
+	}
+	public int getEarnedMarks() {
+		return earnedMarks;
+	}
+	public void setEarnedMarks(int earnedMarks) {
+		this.earnedMarks = earnedMarks;
+	}
+	public String getSubmitterName() {
+		return submitterName;
+	}
+	public void setSubmitterName(String submitterName) {
+		this.submitterName = submitterName;
+	}
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+	
+	
 
 }
