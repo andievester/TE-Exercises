48c9902817384f4349c87c095cb07f99d79fb3a2
Andie Vester <avester146@gmail.com>
2020-06-11 09:18:44 -0400

    Submitting Joins Individual Exercises

--- module-2/03_Joins/student-lecture/postgres/lecture.sql
+++ module-2/03_Joins/student-lecture/postgres/lecture.sql
@@ -1,22 +1,75 @@
 -- ********* INNER JOIN ***********
 
 -- Let's find out who made payment 16666:
+SELECT *
+FROM payment
+WHERE payment_id = 16666;
+
+SELECT *
+FROM customer
+WHERE customer_id = 204;
 
 -- Ok, that gives us a customer_id, but not the name. We can use the customer_id to get the name FROM the customer table
 
+SELECT *
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment_id = 16666;
+
 -- We can see that the * pulls back everything from both tables. We just want everything from payment and then the first and last name of the customer:
+
+SELECT payment.*, customer.first_name, customer.last_name
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+WHERE PAYMENT_ID = 16666;
 
 -- But when did they return the rental? Where would that data come from? From the rental table, so let’s join that.
 
+SELECT payment.*, customer.first_name, customer.last_name
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+INNER JOIN rental ON rental.rental_id = payment.rental_id
+WHERE payment_id = 16666;
+
 -- What did they rent? Film id can be gotten through inventory.
 
--- What if we wanted to know who acted in that film?
+SELECT payment.*, customer.first_name, customer.last_name, rental.*
+FROM payment
+INNER JOIN customer ON customer.customer_id = payment.customer.id
+INNER JOIN rental ON rental.rental_id = payment.rental_id
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN film ON film.film_id = inventory.film_id
+WHERE payment_id = 16666;
 
+-- What if we wanted to know who acted in that film?
+SELECT actor.*, film.title
+FROM film
+INNER JOIN film_actor ON film.film_id = actor.actor_id
+INNER JOIN actor ON film_actor.actor_id = actor.actor_id
+WHERE film.title = 'VOICE PEACH';
 -- What if we wanted a list of all the films and their categories ordered by film title
+
+SELECT film.title, category.name
+FROM film
+INNER JOIN film.category ON tilm_cateogry.film_id = film.film_id
+INNER JOIN category ON film_category.category_id = category.category_id
+ORDER BY film.title ASC;
 
 -- Show all the 'Comedy' films ordered by film title
 
+SELECT film.title, cateogry.name
+FROM film
+INNER JOIN film.category ON tilm_cateogry.film_id = film.film_id
+INNER JOIN category ON film_category.category_id = category.category_id
+WHERE category_name = 'Comedy'
+ORDER BY film.title ASC;
+
 -- Finally, let's count the number of films under each category
+
+SELECT c.name, COUNT(*)
+FROM film_category film_category
+JOIN category c ON c.category_id = fc.category_id
+GROUP BY c.name;
 
 -- ********* LEFT JOIN ***********
 
