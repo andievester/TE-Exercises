d5240ca323b6f38199ca69a941a04adf79836b8f
Andie Vester <avester146@gmail.com>
2020-06-25 12:00:42 -0400

    Submitting ServerSide APIs part 1 Excercises

--- module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -1,6 +1,7 @@
 package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.DAO.AuctionDAO;
+
 import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +17,27 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDAO();
     }
-
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam (required = false, defaultValue = "") String title_like, 
+    		@RequestParam (required = false, defaultValue = "0.0" ) double currentBid_lte) {
+    	String empty = "";
+    	if (!title_like.equals(empty)) {
+    		return dao.searchByTitle(title_like);
+    	}
+    	if (currentBid_lte > 0.0) {
+    		return dao.searchByPrice(currentBid_lte);
+    	}
+    	return dao.list();
+    }
+    
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+    	return dao.get(id);
+    }
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction addAuction (@RequestBody Auction auction) {
+    	return dao.create(auction);
+    }
+    
 }
